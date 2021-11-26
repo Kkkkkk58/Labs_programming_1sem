cdcdeadd76b16c78359e37606f66476327680fde
Karim Khasan <karimhasan3879@gmail.com>
2021-11-26 20:25:33 +0300

    Implemented all required functions [NEEDS RE-REVIEWING]

--- Lab4/id3v2tagger.c
+++ Lab4/id3v2tagger.c
@@ -3,13 +3,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <io.h>
-#include <windows.h>
-#include <mmsystem.h>
 
 
 typedef union ID3v2_header {
     char string_version[10];
-    struct Inner_version{
+    struct Header {
         char tag[3];
         char version;
         char subversion;
@@ -19,6 +17,16 @@ typedef union ID3v2_header {
 } ID3v2_header;
 
 
+typedef union ID3v2_frame {
+    char string_version[10];
+    struct Frame {
+        char frame_name[4];
+        unsigned char size_arr[4];
+        char flags[2];
+    } fields;
+} ID3v2_frame;
+
+
 void show_tags(FILE *song, long header_size, char version);
 void get_tags(FILE *song, char *tag, long header_size, char version);
 void set_tags(FILE *song, char *tag, char *new_value, long header_size, ID3v2_header *header);
@@ -26,6 +34,8 @@ char *get_argument(char *argument);
 void info(char frame_name, char *frame_value, long frame_size);
 void out_image(char *frame_value, long frame_size);
 void get_info(char *frame_name, char *frame_value, long frame_size);
+void change_size(ID3v2_frame *frame, long new_size, int divider);
+
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -113,6 +123,7 @@ int main(int argc, char *argv[]) {
         song = NULL;
         remove(filepath);
         rename("temp.mp3", filepath);
+        printf("Done!");
     }
     if (song != NULL) {
         fclose(song);
@@ -134,28 +145,23 @@ void show_tags(FILE *song, long header_size, char version) {
     }
     int i = 0;
     while (i < header_size) {
-        printf("%d %d", i, header_size);
-        char frame_name[5];   
-        fread(frame_name, sizeof(char), 4, song);
-        if (frame_name[0] == 0) {
+        ID3v2_frame frame;
+        fread(&frame, sizeof(char), 10, song);
+        if (frame.fields.frame_name[0] == 0) {
             return;
         }
+        char frame_name[5];
+        memmove(frame_name, frame.string_version, 4);
         frame_name[4] = '\0';
-        printf("%s\n", frame_name);
-        /* ЕБАНИ СТРУКТУРУ */
-        unsigned char size_arr[4];
-        fread(size_arr, sizeof(char), 4, song);
         long multiplier = degree * degree * degree;
         long frame_size = 0;
         for (int i = 0; i < 4; ++i) {
-            frame_size += size_arr[i] * multiplier;
+            frame_size += frame.fields.size_arr[i] * multiplier;
             multiplier /= degree;
         }
-        char flags[2];
-        fread(flags, sizeof(char), 2, song);
-        char *frame_value =(char *)malloc(frame_size * sizeof(char));
+        char *frame_value = (char *)malloc(frame_size * sizeof(char));
         fread(frame_value, sizeof(char), frame_size, song);
-        // get_info(frame_name, frame_value, frame_size);
+        get_info(frame_name, frame_value, frame_size);
         free(frame_value);
         frame_value = NULL;
         i += 10 + frame_size;
@@ -166,51 +172,38 @@ void show_tags(FILE *song, long header_size, char version) {
 void get_tags(FILE *song, char *tag, long header_size, char version) {
     int i = 0;
     int found = 0;
-    short degree = 128;
+    int degree = 128;
     if (version == 3) {
         degree = 256;
     }
     while (i < header_size) {
-        char frame_name[5];   
-        fread(frame_name, sizeof(char), 4, song);
-        if (frame_name[0] == 0) {
+        ID3v2_frame frame;
+        fread(&frame, sizeof(char), 10, song);
+        if (frame.fields.frame_name[0] == 0) {
             break;
         }
+        char frame_name[5];
+        memmove(frame_name, frame.string_version, 4);
         frame_name[4] = '\0';
         if (strcmp(tag, frame_name) == 0) {
             found = 1;
-            printf("%s ", frame_name);
         }
-        unsigned char size_arr[4];
-        fread(size_arr, sizeof(char), 4, song);
-
         long multiplier = degree * degree * degree;
         long frame_size = 0;
         for (int i = 0; i < 4; ++i) {
-            frame_size += size_arr[i] * multiplier;
+            frame_size += frame.fields.size_arr[i] * multiplier;
             multiplier /= degree;
         }
-        char flags[2];
-        fread(flags, sizeof(char), 2, song);
-        i += 10;
         char *frame_value = (char *)malloc(frame_size * sizeof(char));
         fread(frame_value, sizeof(char), frame_size, song);
         if (found) {
-            int j = 0;
-            if (frame_size > 3 && frame_value[0] == (char)0x1 
-            && frame_value[1] == (char)0xff && frame_value[2] == (char)0xfe) {
-                j = 3;
-            }
-            while (j < frame_size) {
-                printf("%c", frame_value[j]);
-                ++j;
-            }
-            printf("\n");
+            get_info(tag, frame_value, frame_size);
+            free(frame_value);
             return;
         }
         free(frame_value);
         frame_value = NULL;
-        i += frame_size;
+        i += 10 + frame_size;
     } 
     printf("Tag %s not found\n", tag);
 }
@@ -218,128 +211,137 @@ void get_tags(FILE *song, char *tag, long header_size, char version) {
 
 void set_tags(FILE *song, char *tag, char *new_value, long header_size, ID3v2_header *header) {
     int found = 0;
-    int i = 0;
-    short degree = 128;
-    int curr_size = strlen(new_value);
-    if (tag[0] == 'T') {
-        ++curr_size;
-    }
-    int prev_size = 0;
+    long curr_size = strlen(new_value);
+    long prev_size = 0;
     FILE *new_file = fopen("temp.mp3", "wb");
-    int old_header_size = header_size;
+    long old_header_size = header_size;
+    int degree = 128;
     if (header->fields.version == 3) {
         degree = 256;
     }
-    char *buffer = (char *)malloc((header_size + curr_size + 10) * sizeof(char));
-    int iter = 0;
-    while (iter < header_size - 1) {
-        char frame_name[5];   
-        fread(frame_name, sizeof(char), 4, song);
-        if (frame_name[0] == 0) {
+    int divider = degree;
+    char *buffer = (char *)malloc((header_size + curr_size + 10 + 5) * sizeof(char));
+    int buff_index = 0;
+    int iterator = 0;
+    while (iterator < header_size - 1) {
+        ID3v2_frame frame;
+        fread(&frame, sizeof(char), 10, song);
+        if (frame.fields.frame_name[0] == 0) {
             break;
         }
-        frame_name[4] = '\0'; 
+        char frame_name[5];
+        memmove(frame_name, frame.string_version, 4);
+        frame_name[4] = '\0';
         if (strcmp(tag, frame_name) == 0) {
             found = 1;
         }
-        memmove(&buffer[i], frame_name, 4);
-        i += 4;
-        iter += 4;
-        unsigned char size_arr[4];
-        fread(size_arr, sizeof(char), 4, song);
         long multiplier = degree * degree * degree;
         long frame_size = 0;
         for (int j = 0; j < 4; ++j) {
-            frame_size += size_arr[j] * multiplier;
+            frame_size += frame.fields.size_arr[j] * multiplier;
             multiplier /= degree;
         }
         if (found == 1) {
             prev_size = frame_size;
-            long divider = 128;
-            if (header->fields.version == 3) {
-                divider = 256;
+            if (frame_name[0] == 'T') {
+                change_size(&frame, curr_size + 1, divider);  
+                memmove(&buffer[buff_index], frame.string_version, 10);
+                buff_index += 10;
+                memmove(&buffer[buff_index], "\0", 1);
+                buff_index += 1;
             }
-            long tmp = curr_size;
-            for (int j = 0; j < 4; ++j) {
-                size_arr[3 - j] = (char) tmp % divider;
-                tmp /= divider;
-            }        
-        }
-        memmove(&buffer[i], size_arr, 4);
-        i += 4;
-        iter += 4;
-        char flags[2];
-        fread(flags, sizeof(char), 2, song);
-        memmove(&buffer[i], flags, 2);
-        i += 2;
-        iter += 2;
-        if (found == 1) {
-            memmove(&buffer[i], "\0", 1);
-            i += 1;
-            memmove(&buffer[i], new_value, curr_size - 1);
+            else if (strcmp(frame_name, "WXXX") == 0) {
+                change_size(&frame, curr_size + 2, divider);    
+                memmove(&buffer[buff_index], frame.string_version, 10);
+                buff_index += 10;
+                memmove(&buffer[buff_index], "\0\0", 2);
+                buff_index += 2;
+            }
+            else if (strcmp(frame_name, "COMM") == 0 || strcmp(frame_name, "USLT") == 0) {
+                change_size(&frame, curr_size + 5, divider);       
+                memmove(&buffer[buff_index], frame.string_version, 10);
+                buff_index += 10;
+                memmove(&buffer[buff_index], "\0rus\0", 5);
+                buff_index += 5;
+            }
+            else {
+                change_size(&frame, curr_size, divider); 
+                memmove(&buffer[buff_index], frame.string_version, 10);
+                buff_index += 10;
+            }    
+            memmove(&buffer[buff_index], new_value, curr_size);
+            buff_index += curr_size;
             found = 2;
             fseek(song, prev_size, SEEK_CUR);
-            i += curr_size - 1;
+            printf("Changing %s tag value to %s\n", frame_name, new_value);
         }
         else {
+            memmove(&buffer[buff_index], frame.string_version, 10);
+            buff_index += 10;
             char *frame_value = (char *)malloc(frame_size * sizeof(char));
             fread(frame_value, sizeof(char), frame_size, song);
-            memmove(&buffer[i], frame_value, frame_size);
+            memmove(&buffer[buff_index], frame_value, frame_size);
             free(frame_value);
             frame_value = NULL;
-            i += frame_size;
+            buff_index += frame_size;
         }
-    iter += frame_size;
+    iterator += 10 + frame_size;
         
     } 
     if (found == 0) {
-        printf("%c\n", buffer[i]);
-        memmove(&buffer[i], tag, 4);
-        printf("%s\n", tag);
-        i += 4;
-        long divider = 128;
-        if (header->fields.version == 3) {
-            divider = 256;
+        ID3v2_frame frame;
+        memmove(frame.string_version, tag, 4);
+        memmove(frame.fields.flags, "\0\0", 2);
+        if (tag[0] == 'T') {
+            change_size(&frame, curr_size + 1, divider);  
+            memmove(&buffer[buff_index], frame.string_version, 10);
+            buff_index += 10;
+            memmove(&buffer[buff_index], "\0", 1);
+            buff_index += 1;
         }
-        char new_size_arr[4];
-        long tmp = curr_size;
-        for (int j = 0; j < 4; ++j) {
-            new_size_arr[3 - j] = (char) tmp % divider;
-            tmp /= divider;
-        }  
-        memmove(&buffer[i], new_size_arr, 4);
-        i += 4;
-        memmove(&buffer[i], "\0\0", 2);
-        i += 2;
-        // memmove(&buffer[i], "\0", 1);
-        // i += 1;
-        memmove(&buffer[i], new_value, curr_size);
-        i += curr_size;
-        curr_size += 10;
-        
+        else if (strcmp(tag, "WXXX") == 0) {
+            change_size(&frame, curr_size + 2, divider);    
+            memmove(&buffer[buff_index], frame.string_version, 10);
+            buff_index += 10;
+            memmove(&buffer[buff_index], "\0\0", 2);
+            buff_index += 2;
+        }
+        else if (strcmp(tag, "COMM") == 0 || strcmp(tag, "USLT") == 0) {
+            change_size(&frame, curr_size + 5, divider);
+            memmove(&buffer[buff_index], frame.string_version, 10);
+            buff_index += 10;
+            memmove(&buffer[buff_index], "\0rus\0", 5);
+            buff_index += 5;
+        }
+        else {
+            change_size(&frame, curr_size, divider); 
+            memmove(&buffer[buff_index], frame.string_version, 10);
+            buff_index += 10;
+        }
+        memmove(&buffer[buff_index], new_value, curr_size);
+        buff_index += curr_size;
+        printf("Adding %s tag with %s value\n", tag, new_value);
     }
-    header_size = i;
-    int divider = 128;
-    long tmp = header_size;
-    for (i = 0; i < 4; ++i) {
-        header->fields.size_arr[3 - i] = (char) tmp % divider;
-        tmp /= divider;
+    int new_header_size = buff_index;
+    int header_divider = 128;
+    int tmp = new_header_size;
+    for (int i = 0; i < 4; ++i) {
+        header->fields.size_arr[3 - i] = (char) (tmp % header_divider);
+        tmp /= header_divider;
     }
-    for (i = 0; i < 10; ++i) {
+    for (int i = 0; i < 10; ++i) {
         fputc(header->string_version[i], new_file);
     }
-    for (i = 0; i < header_size; ++i) {
+    for (int i = 0; i < new_header_size; ++i) {
         fputc(buffer[i], new_file);
     }
     fflush(new_file);
     free(buffer);
     fseek(song, old_header_size + 10, SEEK_SET);
-    while (!feof(song)) {
-        char c = fgetc(song);
-        fputc(c, new_file);
+    int symbol;
+    while ((symbol = fgetc(song)) != EOF) {
+        fputc(symbol, new_file);
     }
-    char c = fgetc(song);
-    fputc(c, new_file);
     fflush(new_file);
     fclose(new_file);
 }
@@ -399,12 +401,26 @@ void get_info(char *frame_name, char *frame_value, long frame_size) {
             }
             pos = 1;
             while (pos < frame_size) {
-                printf("%c", frame_value[pos]);
+                if (strcmp(frame_name, "USLT") == 0 && frame_value[pos] == '\0') {
+                    printf("\n\t");
+                }
+                else {
+                    printf("%c", frame_value[pos]);
+                }
                 ++pos;
             }
             break;
         case 'W':
-            printf("%s", frame_value);
+            if (strcmp(frame_name, "WXXX") == 0) {
+                if (frame_size > pos + 2 && frame_value[pos] == (char)0x1 
+                && frame_value[pos + 1] == (char)0xff && frame_value[pos + 2] == (char)0xfe) {
+                    pos += 3;
+                }
+            }
+            while (pos < frame_size) {
+                printf("%c", frame_value[pos]);
+                ++pos;
+            }
             break;
         case 'G':
             if (strcmp(frame_name, "GRID") == 0) {
@@ -455,4 +471,12 @@ void out_image(char *frame_value, long frame_size) {
     }
     fflush(image);
     fclose(image);
+}
+
+
+void change_size(ID3v2_frame *frame, long new_size, int divider) {
+    for (int j = 0; j < 4; ++j) {
+        frame->fields.size_arr[3 - j] = (char) (new_size % divider);
+        new_size /= divider;
+    }
 }
